116fab23cf97e4998a68e40fe9830f3ba197584c
DivyaMishra6 <divyamishra599@gmail.com>
2023-07-12 21:52:43 +0530

    [Divya]: ADO Custome Database Created

---
+++ CustomerOperation.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace ADOCustomerProblem
+{
+    public class CustomerOperation
+    {
+
+        public static void CreateDatabase()
+        {
+            SqlConnection con = new SqlConnection("data source=(localdb)\\MSSQLLocalDB; initial catalog=master; integrated security=true");
+            try
+            {
+                //writing sql query
+                string query = "create database DB287";
+                SqlCommand cm = new SqlCommand(query, con);
+
+                //opening connection
+                con.Open();
+
+                //Executing sql query
+                cm.ExecuteNonQuery();
+
+                //Displaying message
+                Console.WriteLine("Database created successfully");
+                Console.WriteLine("------------------------------");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+        public static SqlConnection con = new SqlConnection("data source=(localdb)\\MSSQLLocalDB; initial catalog=DB287; integrated security=true");
+        public static void CreateTable()
+        {
+            try
+            {
+                //writing sql query
+                string query = "create table CudtomerData(Id int identity(1,1)primary key, Name varchar(200), City varchar(200)) ";
+                SqlCommand cm = new SqlCommand(query, con); //we can pass store procedure, query,cant inherit this class
+
+                //opening connection
+                con.Open();
+
+                //Executing sql query
+                cm.ExecuteNonQuery();
+
+                //Displaying message
+                Console.WriteLine("Table created successfully");
+                Console.WriteLine("------------------------------");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        public static void insert()
+        {
+            try
+            {
+                //writing sql query
+                string query = " insert into CudtomerData values ('Divya','Lucknow'),('Priya','Up')";
+                SqlCommand cm = new SqlCommand(query, con);
+
+                //opening connection
+                con.Open();
+
+                //Executing sql query
+                cm.ExecuteNonQuery();
+
+                //Displaying message
+                Console.WriteLine("Data inserted successfully");
+                Console.WriteLine("------------------------------");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        public static void Update()
+        {
+
+        }
+        public static void Delete()
+        {
+
+        }
+        public static void Display()
+        {
+
+        }
+
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ADOCustomerProblem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To ADO.net");
+            // CustomerOperation.CreateDatabase();
+               CustomerOperation.CreateTable();
+            //CustomerOperation.InsertIntoTable();
+        }
+    }
+}
